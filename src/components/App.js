0b07718303c01108c13a8cc30359d3539ca949cb
danielmanfred <danielmanfred@gmail.com>
2019-02-24 23:58:17 -0300

    Relative paths

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Post from './Post'
+import RoutesComponent from './Routes'
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <Post title="I'm learning React" />
+                <Post title="React is very good" />
+                <Post title="Soon I'll learn RN too" />
+                <RoutesComponent />
+            </div>
+        )
+    }
+}
---
+++ src/components/Routes.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import Routes from './../routes'
+
+const RoutesComponent = () => <Routes />
+
+export default RoutesComponent
